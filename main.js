f4a7880d6848e1fea06c0df078b2bf85253295a5
blacksanta87 <ahmadabbaszainol@gmail.com>
2019-02-18 22:08:22 +0800

    add question and answer array

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-alert('Welcome to Quiz Ninja!');
+const quiz = [
+  ['What is Superman\'s real name?', 'Clark Kent'],
+  ['What is Wonder Woman\'s real name?', 'Diana Prince'],
+  ['What is Batman\'s real name?', 'Bruce Wayne']
+];
+
+let score = 0;
+
+for (let [question, answer] of quiz) {
+  let response = prompt(question);
+
+  if (response === answer) {
+    alert('Correct!');
+    score++;
+  } else {
+    alert(`Wrong! The correct answer was ${answer}`);
+  }
+}
+
+alert(`Game over, you scored ${score} point${score !== 1 ? 's' : ''}`);
